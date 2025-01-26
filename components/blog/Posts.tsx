6169575be004b43497d2c769a7c979893fa8b37c
Toyman-tech <adiguno204@gmail.com>
2025-01-26 15:42:04 +0100

    fixed

--- components/blog/Posts.tsx
+++ components/blog/Posts.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Posts = () => {
     return (
         <div className='flex flex-col gap-4'>
-        hello
+        hello   fhnhtrhtrh
         
         </div>
     )
